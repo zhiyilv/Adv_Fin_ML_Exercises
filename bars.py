faf5c596232089e9f896b51ea7b17389f5f06ba3
zhiyi.lu <zhiyilv@outlook.com>
2019-01-07 18:01:58 +0800

    into one file

---
+++ bars.py
@@ -0,0 +1,120 @@
+# import standard libs
+from IPython.display import display
+from IPython.core.debugger import set_trace as bp
+from pathlib import PurePath, Path
+import sys
+import time
+from collections import OrderedDict as od
+import re
+import os
+import json
+os.environ['THEANO_FLAGS'] = 'device=cpu,floatX=float32'
+
+# import python scientific stack
+import pandas as pd
+import pandas_datareader.data as web
+pd.set_option('display.max_rows', 100)
+from dask import dataframe as dd
+from dask.diagnostics import ProgressBar
+pbar = ProgressBar()
+pbar.register()
+import numpy as np
+import scipy.stats as stats
+import statsmodels.api as sm
+from numba import jit
+import math
+import pymc3 as pm
+from theano import shared, theano as tt
+
+# import visual tools
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+# matplotlib inline
+import seaborn as sns
+
+plt.style.use('seaborn-talk')
+plt.style.use('bmh')
+
+#plt.rcParams['font.family'] = 'DejaVu Sans Mono'
+#plt.rcParams['font.size'] = 9.5
+plt.rcParams['font.weight'] = 'medium'
+#plt.rcParams['figure.figsize'] = 10,7
+blue, green, red, purple, gold, teal = sns.color_palette('colorblind', 6)
+
+# import util libs
+import pyarrow as pa
+import pyarrow.parquet as pq
+from tqdm import tqdm, tqdm_notebook
+import warnings
+warnings.filterwarnings("ignore")
+import missingno as msno
+
+from src.utils.utils import *
+from src.features.bars import get_imbalance
+import src.features.bars as brs
+import src.features.snippets as snp
+
+RANDOM_STATE = 777
+
+print()
+
+
+def read_kibot_ticks(fp):
+    # read tick data from http://www.kibot.com/support.aspx#data_format
+    cols = list(map(str.lower, ['Date', 'Time', 'Price', 'Bid', 'Ask', 'Size']))
+    df = (pd.read_csv(fp, header=None)
+          .rename(columns=dict(zip(range(len(cols)), cols)))
+          .assign(dates=lambda df: (pd.to_datetime(df['date'] + df['time'],
+                                                   format='%m/%d/%Y%H:%M:%S')))
+          .assign(v=lambda df: df['size'])  # volume
+          .assign(dv=lambda df: df['price'] * df['size'])  # dollar volume
+          .drop(['date', 'time'], axis=1)
+          .set_index('dates')
+          .drop_duplicates())
+    return df
+
+
+data_dir = Path('./data')
+infp = PurePath(data_dir / 'raw' / 'IVE_tickbidask.txt')
+df = read_kibot_ticks(infp)
+cprint(df)
+
+outfp = PurePath(data_dir/'interim'/'IVE_tickbidask.parq')
+df.to_parquet(outfp)
+
+# can start from here
+# infp=PurePath(data_dir/'interim'/'IVE_tickbidask.parq')
+# df = pd.read_parquet(infp)
+# cprint(df)
+
+# Remove Obvious Price Errors in Tick Data
+sns.boxplot(df.price)  # have a look
+
+
+@jit(nopython=True)
+def mad_outlier(y, thresh=3.):
+    '''
+    compute outliers based on mad
+    # args
+        y: assumed to be array with shape (N,1)
+        thresh: float()
+    # returns
+        array index of outliers
+    '''
+    median = np.median(y)
+    diff = np.sum((y - median)**2, axis=-1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > thresh
+
+
+mad = mad_outlier(df.price.values.reshape(-1, 1))
+df.loc[mad]
+
+# now the data shall be cleared
+sns.boxplot(df.loc[~mad].price)
+
